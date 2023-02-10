36ab845546081d3417b24f4c962fa4cc65f93831
paulinemutuku <p.mutuku@alustudent.com>
2023-02-09 17:00:06 -0800

    Two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for n in range((i+1), 10):
         if i == 8 and n == 9:
-         print(89)
-         break
+            print(89)
+            break
         print("{}{}".format(i, n), end=", ")

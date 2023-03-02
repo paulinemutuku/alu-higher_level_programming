3b3eb8ee23c575157ba9f0020ac6a81fc89a89ad
paulinemutuku <p.mutuku@alustudent.com>
2023-03-02 15:30:39 -0800

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         row_squared = list(map(lambda x: x**2, row))
         new_matrix.append(row_squared)
-    return new_matrix 
+    return new_matrix

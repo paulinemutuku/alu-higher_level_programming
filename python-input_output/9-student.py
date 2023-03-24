d409728e6aa8bf20fd770a8769b7526e81e68138
paulinemutuku <p.mutuku@alustudent.com>
2023-03-24 14:30:39 -0700

    Defines student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-""" class student """
+""" student to json """
 
 
 class Student:
-    """ define function"""
+    """ empty class """
+
     def __init__(self, first_name, last_name, age):
-        """
-        :param self:
-        :param first_name:
-        :param last_name:
-        :param age:
-        """
+        """ just init public instance attributes """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return self.__dict__
-
+        """ return json format """
+        return self.__dict__ 

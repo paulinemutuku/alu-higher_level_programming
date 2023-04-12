6b8c8190d1867ff6156cde5dd41c92e872ff5c36
paulinemutuku <p.mutuku@alustudent.com>
2023-04-12 15:49:44 -0700

    Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" module """
+
+
+def add_integer(a, b=98):
+    """
+    :param a:
+    :param b:
+    :return: a+b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return int(a + b)
+
+
+if __name__ == '__main__':
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)

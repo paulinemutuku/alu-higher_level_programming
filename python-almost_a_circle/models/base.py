ae3a9bef2e1cfcdf47f1f7653bc5ef6f86279b1b
paulinemutuku <p.mutuku@alustudent.com>
2023-04-28 13:00:49 -0700

    Last commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,93 +1,113 @@
 #!/usr/bin/python3
-""" new class called base """
+"""``Base`` class module"""
+
 import json
-import csv
+import os
 
 
 class Base:
-    """ define Base with id """
+    """Base of all classes in this project"""
 
     __nb_objects = 0
 
-    def __init__(self, id=None):
-        """ method initialization """
-        if id is not None:
-            self.id = id
-        else:
+    def __init__(self, _id=None):
+        """Sets private attributes and id fields
+            Args:
+                _id (int): A integer
+        """
+        if _id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = _id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ JSON string representation """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return '[]'
-        else:
-            return json.dumps(list_dictionaries)
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        elif type(list_dictionaries) is not list:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
 
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """ writes the JSON string representation """
-        d = []
-        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
-            if list_objs:
-                for obj in list_objs:
-                    d.append(obj.to_dictionary())
-            f.write(cls.to_json_string(d))
+        for d in list_dictionaries:
+            if type(d) is not dict:
+                msg = "list_dictionaries must be a list of dictionaries"
+                raise TypeError(msg)
 
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
     def from_json_string(json_string):
-        """ returns the list of the JSON string repr """
-        if not json_string:
+        """Returns the list of the JSON string representation"""
+        if json_string is None:
             return []
         return json.loads(json_string)
 
     @classmethod
-    def create(cls, **dictionary):
-        """ returns an instance with all attributes """
-        if cls.__name__ == 'Rectangle':
-            dummy = cls(1, 1)
-        if cls.__name__ == 'Square':
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_obj"""
+        if list_objs is None:
+            list_objs = []
+
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string([o.to_dictionary() for o in list_objs]))
 
     @classmethod
     def load_from_file(cls):
-        """ serializes """
-        try:
-            with open(cls.__name__ + ".json", "r") as f:
-                return [cls.create(**dictionary) for
-                        dictionary in cls.from_json_string(f.read())]
-        except FileNotFoundError:
+        """Returns a list of instances"""
+        filename = "{}.json".format(cls.__name__)
+
+        if not os.path.exists(filename):
             return []
+
+        ret = []
+        with open(filename, "r", encoding="utf-8") as f:
+            list_dicts = cls.from_json_string(f.read())
+            ret = [cls.create(**d) for d in list_dicts]
+        return ret
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """serializes and deserializes in CSV"""
-        ld = []
-        with open(cls.__name__ + ".csv", "w", encoding="utf-8") as f:
-            if list_objs:
-                for obj in list_objs:
-                    if cls.__name__ == 'Rectangle':
-                        ld.append([
-                            obj.id, obj.width, obj.height, obj.x, obj.y])
-                    if cls.__name__ == 'Square':
-                        ld.append([obj.id, obj.size, obj.x, obj.y])
-            writer = csv.writer(f)
-            for row in ld:
-                writer.writerow(row)
+        """Serializes to csv a list of instances"""
+        if list_objs is None:
+            list_objs = []
+
+        filename = "{}.csv".format(cls.__name__)
+        attrs = ('id', 'size', 'width', 'height', 'x', 'y')
+        with open(filename, "w", encoding="utf-8") as f:
+            for o in list_objs:
+                d = o.to_dictionary()
+                t = []
+                for attr in attrs:
+                    if attr not in d:
+                        continue
+                    t.append(str(d.get(attr)))
+                f.write(",".join(t))
+                f.write("\n")
 
     @classmethod
     def load_from_file_csv(cls):
-        """serializes and deserializes in CSV """
-        try:
-            with open(cls.__name__ + ".csv", "r") as f:
-                ld = []
-                reader = csv.DictReader(f)
-                for row in reader:
-                    for key, val in row.items():
-                        row[key] = int(val)
-                ld.append(row)
-                return [cls.create(**item) for item in ld]
-        except FileNotFoundError:
-                return []
+        """deserializes CSV"""
+        filename = "{}.csv".format(cls.__name__)
+
+        if not os.path.exists(filename):
+            return []
+
+        list_objs = []
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                arguments = line[:-1].split(",")
+                o = cls(1, 1)
+                o.update(*[int(x) for x in arguments])
+                list_objs.append(o)
+        return list_objs
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        instance = cls(1, 1)
+        instance.x = 0
+        instance.y = 0
+        instance.update(**dictionary)
+        return instance

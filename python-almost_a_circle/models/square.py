95fb81754a486564f66d848da7dad18b953b34e5
paulinemutuku <p.mutuku@alustudent.com>
2023-04-28 13:12:29 -0700

    Last commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,52 +1,72 @@
 #!/usr/bin/python3
-"""
-class Square
-"""
-from .rectangle import Rectangle
+"""square module"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """docstring for ."""
+    """defines the class square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ class constructor"""
+        """initializes a square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Defines print attribute """
-        attributes = "[Square] ({}) {}/{} - {}"
-        return attributes.format(self.id, self.x, self.y, self.width)
+        """string representation of a square"""
+        r = "[{}] ({}) ".format(self.__class__.__name__, self.id)
+        c = "{}/{} - {}".format(self.x, self.y, self.width)
+        return r + c
 
     @property
     def size(self):
-        """ Getter of the size attribute """
+        """getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ setter of the size attribute """
+        """setter for size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """method that assigns attributes"""
-        if len(args):
-            i = 0
-            keys_of_attributes = ['id', 'size', 'x', 'y']
-            for value in args:
-                if i < 4:
-                    setattr(self, keys_of_attributes[i], value)
-                    i += 1
+        """update square that assigns attributes to args"""
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    if args[i] is None:
+                        self.__init__(
+                                self.width, self.width, self.x, self.y)
+                    else:
+                        self.id = args[i]
+                if i == 1:
+                    self.width = args[i]
+                    self.height = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
         else:
-            for keys_of_attributes, value in kwargs.items():
-                setattr(self, keys_of_attributes, value)
+            if kwargs and len(kwargs) != 0:
+                for key, value in kwargs.items():
+                    if key == "id":
+                        if value is None:
+                            self.__init__(
+                                    self.width, self.width, self.x, self.y)
+                        else:
+                            self.id = value
+                    elif key == "size":
+                        self.width = value
+                        self.height = value
+                    elif key == "x":
+                        self.x = value
+                    elif key == "y":
+                        self.y = value
 
     def to_dictionary(self):
-        """ Returns a square dictionary """
-        dic = {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-            }
-        return dic
+        """returns dictionary representation
+        of a square"""
+        return {"id": self.id, "size": self.size,
+                "x": self.x, "y": self.y}

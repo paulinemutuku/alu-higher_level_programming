7fd5d79f81f2ee3b2df2af36f3db033024d1d6fc
paulinemutuku <p.mutuku@alustudent.com>
2023-06-16 05:03:10 -0700

    Output is multiple lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const myProject = 'Python is cool';
+const myLife = 'JavaScript is amazing';
+console.log(myVar);
+console.log(myProject);
+console.log(myLife);

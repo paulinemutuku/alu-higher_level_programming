b68b8f77433650b66f776d1f6074bb32bb620318
paulinemutuku <p.mutuku@alustudent.com>
2023-06-16 07:08:38 -0700

    Getting a URL status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+
+request(url, (err, response, bosy) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
